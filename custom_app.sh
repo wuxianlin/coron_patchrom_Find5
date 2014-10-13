3d50b43572a5b0cc91c8e5c4fbddaa10fd321c9d
wuxianlin <wuxianlinwxl@gmail.com>
2014-10-13 23:50:44 +0800

    fix Contacts permission

--- custom_app.sh
+++ custom_app.sh
@@ -11,6 +11,10 @@ elif [ "$apkBaseName" = "Keyguard" ];then
         echo ">>> in custom_app for $apkBaseName to add some permissions for android 4.4.4"
 	sed -i '/<uses-permission android:name="android.permission.VIBRATE" \/>/a\<uses-permission android:name="android.permission.ACCESS_KEYGUARD_SECURE_STORAGE" \/>' $tempSmaliDir/AndroidManifest.xml
 
+elif [ "$apkBaseName" = "Contacts" ];then
+        echo ">>> in custom_app for $apkBaseName to add some permissions for android 4.4.4"
+	sed -i '/<original-package android:name="com.android.contacts" \/>/a\<uses-permission android:name="android.permission.SET_PREFERRED_APPLICATIONS" \/>' $tempSmaliDir/AndroidManifest.xml
+
 elif [ "$apkBaseName" = "Settings" ];then
     echo ">>> in custom_app $apkBaseName"
 
